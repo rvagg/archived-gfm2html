8441aaf94ddedd9123aa251dd41a73964add97a2
Christopher Fredén <c.freden@gmail.com>
2015-02-23 17:20:57 +0100

    Added stdin/stdout support by using - instead of file names

--- bin.js
+++ bin.js
@@ -3,22 +3,42 @@
 const gfm2html = require('./')
     , fs       = require('fs')
 
+var input = ''
+
 function usageAndExit () {
   console.error('Usage: gfm2html <markdown file> <output file>')
   process.exit(-1)
 }
 
+function finish(input) {
+  gfm2html(input, function (err, data) {
+    if (err)
+      throw err
+
+    if (process.argv[3] === '-') {
+      process.stdout.write(data);
+    } else {
+      fs.writeFileSync(process.argv[3], data, 'utf8')
+    }
+  })
+}
+
 if (process.argv.length < 4)
   return usageAndExit()
 
-if (!fs.existsSync(process.argv[2])) {
-  console.error('File "' + process.argv[2] + '" does not exist')
-  return usageAndExit()
+if (process.argv[2] === '-') {
+  process.stdin.on('readable', function() {
+    var chunk = process.stdin.read();
+    if (chunk !== null) input += chunk;
+  });
+
+  process.stdin.on('end', function() {
+    finish(input)
+  });
+} else {
+  if (!fs.existsSync(process.argv[2])) {
+    console.error('File "' + process.argv[2] + '" does not exist')
+    return usageAndExit()
+  }
+  finish(fs.readFileSync(process.argv[2], 'utf8'))
 }
-
-gfm2html(fs.readFileSync(process.argv[2], 'utf8'), function (err, data) {
-  if (err)
-    throw err
-
-  fs.writeFileSync(process.argv[3], data, 'utf8')
-})
